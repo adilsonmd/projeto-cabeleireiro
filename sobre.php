4ec3ab58283c04dbf3574af4aaa122af23d22679
Adilson Alves <adilson.day@hotmail.com>
2016-06-06 16:11:34 -0300

    Ajustes

--- sobre.php
+++ sobre.php
@@ -18,15 +18,15 @@
             <?php
             include("menu.php");
             ?>
-        </div>
-        
-        <div class="mdl-grid">
-            <div class="mdl-cell mdl-cell--2-col"></div>
-            <div id="content" class="mdl-cell mdl-cell--8-col">
-            	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3646.089079054121!2d-46.32519908483111!3d-23.957289982217844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce03a0c9fb6053%3A0x46ee11fc8292f606!2sAv.+Conselheiro+N%C3%A9bias%2C+536+-+Paquet%C3%A1%2C+Santos+-+SP%2C+11015-001!5e0!3m2!1spt-BR!2sbr!4v1465238600691" width="600" height="450" frameborder="0" style="border:0" allowfullscreen>
-            	</iframe>
+            <div class="mdl-grid">
+                <div class="mdl-cell mdl-cell--2-col"></div>
+                <div id="content" class="mdl-cell mdl-cell--8-col">
+            	    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3646.089079054121!2d-46.32519908483111!3d-23.957289982217844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce03a0c9fb6053%3A0x46ee11fc8292f606!2sAv.+Conselheiro+N%C3%A9bias%2C+536+-+Paquet%C3%A1%2C+Santos+-+SP%2C+11015-001!5e0!3m2!1spt-BR!2sbr!4v1465238600691" width="600" height="450" frameborder="0" style="border:0" allowfullscreen>
+            	    </iframe>
+                </div>
+                <div class="mdl-cell mdl-cell--2-col"></div>
             </div>
-            <div class="mdl-cell mdl-cell--2-col"></div>
         </div>
+
     </body>
 </html>
