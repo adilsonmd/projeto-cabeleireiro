9a15547aca2e75cdd3ab41000fecf1b40b937d60
Adilson Alves <adilson.day@hotmail.com>
2016-06-06 21:17:46 -0300

    margens ajustadas

--- catalogo.php
+++ catalogo.php
@@ -10,6 +10,9 @@
     <link rel="stylesheet" href="css/estilo.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
+        .mdl-grid {
+            margin: 10px 0 10px 0;
+        }
         .teste {
             background-color: #222;
             color:white;
