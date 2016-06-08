64b13d616d6b6bc798b79e53b28441cb62a353f0
Adilson Alves <adilson.day@hotmail.com>
2016-06-07 22:19:45 -0300

    agora vai

--- catalogo.php
+++ catalogo.php
@@ -10,9 +10,7 @@
     <link rel="stylesheet" href="css/estilo.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
-        .mdl-grid {
-            margin: 10px 0 10px 0;
-        }
+        
         .mdl-cell {
             box-sizing: border-box;
             background-color: #fcfcfc;
