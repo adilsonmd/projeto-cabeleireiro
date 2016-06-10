e7c13654140334962fb1e536729567a327cd6652
Adilson Alves <adilson.day@hotmail.com>
2016-06-09 23:45:57 -0300

    espaço 1

--- catalogo.php
+++ catalogo.php
@@ -58,9 +58,11 @@
                 
             <div class="mdl-cell mdl-cell--4-col-desktop img-cell"><img src="imagens/catalogo/corte1.jpg" id="img1"><!-- conteúdo --></div>
                 
-            <div class="mdl-cell mdl-cell--2-col-desktop mdl-cell--hide-phone"></div> <!-- Espaçamento -->
+            <div class="mdl-cell mdl-cell--1-col-desktop mdl-cell--hide-phone"></div> <!-- Espaçamento -->
                 
             <div class="mdl-cell mdl-cell--4-col-desktop img-cell"><img src="imagens/catalogo/corte2.jpg" id="img1"></div> <!-- Espaçamento -->
+
+            <div class="mdl-cell mdl-cell--1-col-desktop mdl-cell--hide-phone"></div> <!-- Espaçamento -->
 
         </div>
 
@@ -69,9 +71,11 @@
                 
             <div class="mdl-cell mdl-cell--4-col-desktop img-cell"><img src="imagens/catalogo/corte3.jpg" id="img1"> <!-- conteúdo --></div>
                 
-            <div class="mdl-cell mdl-cell--2-col-desktop mdl-cel--hide-phone"></div> <!-- Espaçamento -->
+            <div class="mdl-cell mdl-cell--1-col-desktop mdl-cel--hide-phone"></div> <!-- Espaçamento -->
                 
             <div class="mdl-cell mdl-cell--4-col-desktop img-cell"><img src="imagens/catalogo/corte4.jpg" id="img1"></div> <!-- Espaçamento -->
+
+            <div class="mdl-cell mdl-cell--1-col-desktop mdl-cell--hide-phone"></div> <!-- Espaçamento -->
 
         </div>
 
@@ -80,9 +84,11 @@
                 
             <div class="mdl-cell mdl-cell--4-col-desktop img-cell">imagem <!-- conteúdo --></div>
                 
-            <div class="mdl-cell mdl-cell--2-col-desktop mdl-cell--hide-phone">imagem</div> <!-- Espaçamento -->
+            <div class="mdl-cell mdl-cell--1-col-desktop mdl-cell--hide-phone">imagem</div> <!-- Espaçamento -->
                 
             <div class="mdl-cell mdl-cell--4-col-desktop img-cell">imagem</div> <!-- Espaçamento -->
+
+            <div class="mdl-cell mdl-cell--1-col-desktop mdl-cell--hide-phone"></div> <!-- Espaçamento -->
 
         </div>
         
