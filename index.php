ba25761fa912110d0e221b7d46a6c6260f056ed2
Adilson Alves <adilson.day@hotmail.com>
2016-06-10 01:28:11 -0300

    toast no index

--- index.php
+++ index.php
@@ -10,6 +10,16 @@
 		<script defer src="mdl/material.min.js"></script>
 		<link rel="stylesheet" href="css/estilo.css" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<script>
+		$(document).ready(function() {
+			'use strict';
+			var snackbarContainer = $('#demo-toast-example');
+			
+			var data = {message: 'Para uma melhor experiência use o navegador Google Chrome.'};
+				snackbarContainer.MaterialSnackbar.showSnackbar(data);
+			});
+		}());
+		</script>
 
 	</head>
 
@@ -25,6 +35,12 @@
 			<div id="content" class="mdl-layout__content">
 				<h3 class="mdl-layout__title">Página inicial</h3>
 			</div>
+			
+			<button id="demo-show-snackbar" class="mdl-button mdl-js-button mdl-button--raised" type="button">Show Snackbar</button>
+			<div id="demo-snackbar-example" class="mdl-js-snackbar mdl-snackbar">
+			<div class="mdl-snackbar__text"></div>
+			
+			
 
 			<?php
             include("footer.html");
