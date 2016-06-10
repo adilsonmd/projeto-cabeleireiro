1d72bde665a76c03df4d268f9b0bd321b554fb2f
Adilson Alves <adilson.day@hotmail.com>
2016-06-09 21:55:11 -0300

    melhoria bakcground

--- catalogo.php
+++ catalogo.php
@@ -29,6 +29,8 @@
 
         #img1 {
             background-image: url("imagens/catalogo/corte1.jpg"); 
+            background-repeat: no-repeat;
+            background-size: cover;
         }
     </style>
 </head>
