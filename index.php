658d4df0e41d2e28a1bebbe95fa5c10c0fbe8bdd
pattycuti <wenndy.San@hotmail.com>
2016-06-13 14:30:38 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 		<meta charset="UTF-8">
 		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
 		<link rel="stylesheet" href="mdl/material.red-orange.min.css" />
+		<link rel="stylesheet" href="css/cabelo.css" />
 		<script defer src="mdl/material.min.js"></script>
 		<link rel="stylesheet" href="css/estilo.css" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
